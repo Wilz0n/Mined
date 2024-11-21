92fc664ddbf16a5a446f4018cf76a4aee6164c3b
javier bernal <fishas02@gmail.com>
2024-11-21 11:46:41 -0500

    inicio de proyecto de guia para mined

--- src/componets/atoms/Box.jsx
+++
@@ -1,9 +0,0 @@
-// src/components/atoms/Box.jsx
-import React from 'react';
-import Box from '@mui/material/Box';
-
-const CustomBox = ({ children, ...props }) => {
-  return <Box {...props}>{children}</Box>;
-};
-
-export default CustomBox;
--- src/componets/atoms/Typography.jsx
+++
@@ -1,9 +0,0 @@
-// src/components/atoms/Typography.jsx
-import React from 'react';
-import Typography from '@mui/material/Typography';
-
-const CustomTypography = ({ children, ...props }) => {
-  return <Typography {...props}>{children}</Typography>;
-};
-
-export default CustomTypography;
